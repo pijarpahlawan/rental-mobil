6bc7e413cf7c6e9faeebf32857d8eb13b8941d34
Muhammad Zhafar Al Fathi <95948333+zha-farr@users.noreply.github.com>
2021-12-20 18:46:56 +0700

    Add files via upload

---
+++ src/RandomData.cpp
@@ -0,0 +1,53 @@
+#include "iostream"
+#include "ctime"
+#include "stdlib.h"
+#include "sstream"
+#include "DataMobil.h"
+
+using namespace std;
+
+string modelRandom[15];
+string bensin[15];
+string kursi[15];
+string cc[15];
+int harga[15];
+string warna[15];
+string adaAC[15];
+string tipeMesin[15];
+string bahanbakar[15];
+int kilometer[15];
+string platNomor[15];
+bool tersedia[15];
+
+string IntToString(int a)
+{
+    ostringstream temp;
+    temp << a;
+    return temp.str();
+}
+
+void generateRandomData()
+{   
+    srand((unsigned int)time(NULL));
+    for (int i = 0; i < 15; i++)
+    {
+        int r = rand() % (sizeof(models) / sizeof(string));
+        modelRandom[i] = models[rand() % 36];
+        bensin[i] = fuels[r];
+        kursi[i] = seats[r];
+        cc[i] = engines[r];
+        harga[i] = prices[r];
+        warna[i] = rand() % 2 == 1 ? colors[r] : allColors[rand() % 108];
+        adaAC[i] = haveAC[r];
+        tipeMesin[i] = engineTypes[r];
+        bahanbakar[i] = fuelTypes[r];
+        kilometer[i] = rand() % 98888 + 1111;
+        tersedia[i] = rand() % 10 != 1;
+        string platDaerah = awalanPlat[rand() % (sizeof(awalanPlat) / sizeof(string))];
+        int nomorPlat = rand() % 8999 + 1000;
+        char akhiran1 = rand() % 26 + 65;
+        char akhiran2 = rand() % 26 + 65;
+        platNomor[i] = platDaerah + IntToString(nomorPlat) + akhiran1 + akhiran2;
+        cout << modelRandom[i] << " Plat: " << platNomor[i] << " Tersedia: " << tersedia[i] << endl;
+    }
+}
