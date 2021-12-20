040f2a742c272e84f9e079b6fa66d72c6d111791
pijar <pijarpijarpahlawanq@students.amikom.ac.id>
2021-12-20 22:18:51 +0700

    update komentar

--- src/RandomData.h
+++ src/RandomData.h
@@ -1,18 +1,60 @@
-#pragma once
-#include "iostream"
+#include <iostream>
+#include <ctime>
+#include <stdlib.h>
+#include <sstream>
+#include "DataMobil.h"
+
 using namespace std;
 
-extern string modelRandom[15];
-extern string bensin[15];
-extern string kursi[15];
-extern string cc[15];
-extern int harga[15];
-extern string warna[15];
-extern string adaAC[15];
-extern string tipeMesin[15];
-extern string bahanbakar[15];
-extern int kilometer[15];
-extern string platNomor[15];
-extern bool tersedia[15];
-void generateRandomData();
-string IntToString(int a);
+//array tempat menyimpan hasil randomisasi
+string modelRandom[15];
+string bensin[15];
+string kursi[15];
+string cc[15];
+int harga[15];
+string warna[15];
+string adaAC[15];
+string tipeMesin[15];
+string bahanbakar[15];
+int kilometer[15];
+string platNomor[15];
+bool tersedia[15];
+
+//mengubah plat nomor menjadi string
+string IntToString(int a)
+{
+    ostringstream temp; //template character
+    temp << a;          //formating a to char array
+    return temp.str();  //returning <a> to string value
+}
+
+//merandom mobil, spesifikasi, dan harganya
+void generateRandomData()
+{
+    srand((unsigned int)time(NULL)); //seed rand()
+
+    //mengambil 15 data
+    for (int i = 0; i < 15; i++)
+    {
+        /*mendapatkan indeks ke-r array sebelum terandom. berbasis array models[]*/
+        int r = rand() % (sizeof(models) / sizeof(string));
+
+        //randomization
+        modelRandom[i] = models[r];
+        bensin[i] = fuels[r];
+        kursi[i] = seats[r];
+        cc[i] = engines[r];
+        harga[i] = prices[r];
+        warna[i] = rand() % 2 == 1 ? colors[r] : allColors[rand() % 108];
+        adaAC[i] = haveAC[r];
+        tipeMesin[i] = engineTypes[r];
+        bahanbakar[i] = fuelTypes[r];
+        kilometer[i] = rand() % 98888 + 1111;
+        tersedia[i] = rand() % 10 != 1;
+        string platDaerah = awalanPlat[rand() % (sizeof(awalanPlat) / sizeof(string))];
+        int nomorPlat = rand() % 8999 + 1000;
+        char akhiran1 = rand() % 26 + 65;
+        char akhiran2 = rand() % 26 + 65;
+        platNomor[i] = platDaerah + IntToString(nomorPlat) + akhiran1 + akhiran2;
+    }
+}
