071fafa2ea0ee74ff21bca832926f15e94b24f45
pijar <pijarpijarpahlawanq@students.amikom.ac.id>
2021-12-21 21:05:00 +0700

    menambahkan variabel nomor rekening

--- src/RandomData.h
+++ src/RandomData.h
@@ -11,29 +11,30 @@ string modelRandom[15];
 string bensin[15];
 string kursi[15];
 string cc[15];
-int harga[15];
 string warna[15];
 string adaAC[15];
 string tipeMesin[15];
 string bahanbakar[15];
-int kilometer[15];
+int harga[15];
 string platNomor[15];
+string nomorRekening;
+int kilometer[15];
 bool tersedia[15];
 
-//mengubah plat nomor menjadi string
+//mengubah integer menjadi string
 string IntToString(int a)
 {
     ostringstream temp; //template character
-    temp << a;          //formating a to char array
+    temp << a;          //formating a to char array (string)
     return temp.str();  //returning <a> to string value
 }
 
-//merandom mobil, spesifikasi, dan harganya
+//merandom data
 void generateRandomData()
 {
     srand((unsigned int)time(NULL)); //seed rand()
 
-    //mengambil 15 data
+    //mengambil 15 data mobil, spesifikasi, dan harganya
     for (int i = 0; i < 15; i++)
     {
         /*mendapatkan indeks ke-r array sebelum terandom. berbasis array models[]*/
@@ -56,5 +57,19 @@ void generateRandomData()
         char akhiran1 = rand() % 26 + 65;
         char akhiran2 = rand() % 26 + 65;
         platNomor[i] = platDaerah + IntToString(nomorPlat) + akhiran1 + akhiran2;
+    }
+
+    //merandom nomor rekening
+    for (int i = 0; i < 5; i++)
+    {
+        int noRek = rand() % 999 + 11;
+        if (i == 0)
+        {
+            nomorRekening = IntToString(noRek);
+        }
+        else
+        {
+            nomorRekening = nomorRekening + IntToString(noRek);
+        }
     }
 }
