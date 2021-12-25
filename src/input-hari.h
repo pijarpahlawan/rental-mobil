f07fbcd0958dbd404920851d237b318acdf08b50
pijar <pijarpijarpahlawanq@students.amikom.ac.id>
2021-12-26 02:24:56 +0700

    memperbaiki bug saat memasukkan jumlah hari

--- src/input-hari.h
+++ src/input-hari.h
@@ -5,48 +5,52 @@ bool asked = false;
 void Ulang()
 {
     asked = false;
-    cout << "Tekan ENTER untuk mengulangi";
     cin.clear();
     cin.ignore();
-    cin.ignore();
-    cin.get();  
-    system("cls");    
+    cout << "Tekan ENTER untuk mengulangi";
+    cin.get();
+    system("cls");
 }
 
-void InputHari()
+int main()
 {
     int jmlh_hari = 0;
-    char y_n;
+    char *jmlHari_str;
+    char y_n[] = "y/n";
     bool isRun = true;
     while (isRun)
     {
-        baleni:
+    baleni:
         system("cls");
         cout << "Anda mau meminjam untuk berapa hari: ";
-        if ((cin >> jmlh_hari) && jmlh_hari > 0)
+        cin >> jmlHari_str;
+        /*sscanf() berfungsi untuk mengcasting tipe data string ke integer*/
+        if (sscanf(jmlHari_str, "%d", &jmlh_hari) && jmlh_hari > 0)
         {
             cout << endl;
             cout << "Apakah anda yakin, ketik y/n ";
+            cin >> y_n;
             asked = true;
-        } 
+        }
         else
         {
             cout << "Silahkan masukkan bilangan bulat positif, dilain itu akan gagal" << endl;
             Ulang();
             goto baleni;
-        }               
-        if ((cin >> y_n) && (y_n == 'y' || y_n == 'Y'))
+        }
+        if ((sizeof(y_n) / sizeof(char) == 4) && (y_n[0] == 'y' || y_n[0] == 'Y'))
         {
             cout << "Menampilkan biaya untuk " << jmlh_hari << " hari: ";
             isRun = false;
         }
         else
         {
-            if (!(y_n == 'n' || y_n == 'N'))
+            if ((sizeof(y_n) / sizeof(char) == 4) && !(y_n[0] == 'n' || y_n[0] == 'N'))
             {
                 cout << "Silahkan masukkan y/n, dilain itu salah" << endl;
             }
             Ulang();
-        }       
+        }
     }
+    return jmlh_hari;
 }
