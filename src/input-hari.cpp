f3533e7b1f5062d5db4d7cdc0238d1cf07aa6dc0
pijar <pijarpijarpahlawanq@students.amikom.ac.id>
2021-12-26 10:30:39 +0700

    menghapus variabel asked

--- src/input-hari.cpp
+++ src/input-hari.cpp
@@ -1,15 +1,12 @@
 #include <iostream>
 using namespace std;
-bool asked = false;
 
 void Ulang()
 {
-    asked = false;
     cin.clear();
+    cout << "Tekan ENTER untuk mengulangi...";
     cin.ignore();
-    cout << "Tekan ENTER untuk mengulangi";
     cin.get();
-    system("cls");
 }
 
 int main()
@@ -18,9 +15,9 @@ int main()
     char *jmlHari_str;
     char y_n[] = "y/n";
     bool isRun = true;
+
     while (isRun)
     {
-    baleni:
         system("cls");
         cout << "Anda mau meminjam untuk berapa hari: ";
         cin >> jmlHari_str;
@@ -30,27 +27,26 @@ int main()
             cout << endl;
             cout << "Apakah anda yakin, ketik y/n ";
             cin >> y_n;
-            asked = true;
-        }
-        else
-        {
-            cout << "Silahkan masukkan bilangan bulat positif, dilain itu akan gagal" << endl;
-            Ulang();
-            goto baleni;
-        }
-        if ((sizeof(y_n) / sizeof(char) == 4) && (y_n[0] == 'y' || y_n[0] == 'Y'))
-        {
-            cout << "Menampilkan biaya untuk " << jmlh_hari << " hari: ";
-            isRun = false;
-        }
-        else
-        {
-            if ((sizeof(y_n) / sizeof(char) == 4) && !(y_n[0] == 'n' || y_n[0] == 'N'))
+            if ((sizeof(y_n) / sizeof(char) == 4) && (y_n[0] == 'y' || y_n[0] == 'Y'))
             {
-                cout << "Silahkan masukkan y/n, dilain itu salah" << endl;
+                cout << "Menampilkan biaya untuk " << jmlh_hari << " hari: ";
+                isRun = false;
             }
+            else
+            {
+                if ((sizeof(y_n) / sizeof(char) == 4) && !(y_n[0] == 'n' || y_n[0] == 'N'))
+                {
+                    cout << "ERROR:silahkan masukkan y/n, dilain itu salah." << endl;
+                }
+                Ulang();
+            }
+        }
+        else
+        {
+            cout << "ERROR:silahkan masukkan bilangan bulat positif, dilain itu akan gagal." << endl;
             Ulang();
         }
     }
+
     return jmlh_hari;
 }
