1332236b2a6d918b922d0d922a1c9df7a70848ac
pijar <pijarpijarpahlawanq@students.amikom.ac.id>
2022-01-05 08:21:14 +0700

    removing some documentation

--- src/main.cpp
+++ src/main.cpp
@@ -25,9 +25,9 @@ int main()
 
     /* control flow display models */
 menu:
-    currentGarage = 1;          // variabel untuk menampung garasi ke-
-    doneInteraction = false;    // variabel sebagai kondisi apakah program masih berjalan atau telah selesai
-    reprint = true;             // variabel sebagai kondisi apakah program masih mencetak daftar mobil atau tidak
+    currentGarage = 1;          
+    doneInteraction = false;    
+    reprint = true;             
     while (true)
     {
         if (reprint)
